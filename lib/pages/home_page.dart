e51191c929a327e5f1544616ac74488eec30a015
darknarf64 <f3e64@hotmail.com.com>
2021-12-03 18:12:18 -0500

    3-12 1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,24 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:lector_qr_sqllite/widgets/custom_navigatorbar.dart';
+import 'package:lector_qr_sqllite/widgets/scan_button.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('historial'),
+        actions: [
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
+      ),
       body: Center(
         child: Text('Home Page'),
       ),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
